e2dc5be425c51f86fb2c025d87febca765c3ea4d
yaoyihong <2427182935@qq.com>
2022-12-25 23:28:03 +0800

    一个网易云项目
    https://www.bilibili.com/video/BV1c44y1g7ac/?spm_id_from=333.999.0.0

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
+import getVant from './plugins'
+const app=createApp(App)
+getVant(app)
+app.use(store)
+app.use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <TopNav></TopNav>
+  <SwiperTop></SwiperTop>
+  <IconList></IconList>
+  <MusicList></MusicList>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import TopNav from '@/components/home/TopNav.vue'
+import SwiperTop from '@/components/home/SwiperTop.vue'
+import IconList from '@/components/home/IconList.vue'
+import MusicList from '@/components/home/MusicList.vue'
+ // @ is an alias to /src
+
+@Options({
+  components: {
+    TopNav,SwiperTop,IconList,MusicList
+  },
+})
+export default class HomeView extends Vue {}
+</script>
+
+
